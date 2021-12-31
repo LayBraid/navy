41f53dfeed46a9870e13864a1fc72068940f1bcd
Timothe MEDICO <medico.timothe@gmail.com>
2021-12-31 14:19:20 +0100

    error handling

--- src/main.c
+++ src/main.c
@@ -7,8 +7,27 @@
 
 #include "navy.h"
 
+void put_error(char const *message)
+{
+    write(1, message, my_strlen(message));
+}
+
+void print_usage(int size)
+{
+    char *value = my_itoa(size);
+
+    put_error("./my_radar: bad arguments: ");
+    put_error(value);
+    put_error(" given but 1 is required\nretry with -h\n");
+    free(value);
+}
+
 int main(int size, char **args)
 {
-    launch(size, args);
-    return 0;
+    if(size == 2 || size == 3) {
+        return launch(size, args);
+    } else {
+        print_usage(size - 1);
+        return 84;
+    }
 }
--- src/navy.c
+++ src/navy.c
@@ -51,29 +51,27 @@ int help_message()
               "DESCRIPTION\n     first_player_pid: only for the 2nd player."
               " pid of the first player.\n     navy_positions: file "
               "representing the positions of the ships.\n");
-    return 1;
 }
 
 int launch(int size, char **args)
 {
-    if (size == 2 && my_strcmp(args[1], "-h") == 0)
-        return help_message();
-    if (size == 2 || size == 3) {
-        navy = malloc(sizeof(navy_t));
-        navy->bool_game = 0;
-        navy->enemy_pid = 0;
-        navy->attack = malloc(sizeof(attack_t));
-        navy->attack->attack = malloc(sizeof(int) * 2);
-        handle_signal(SIGUSR1);
-        handle_signal(SIGUSR2);
-        fill_map(args[size - 1]);
-        if (size == 2) {
-            first_player();
-        } else {
-            navy->enemy_pid = my_atoi(args[1]);
-            second_player();
-        }
-        return 0;
+    if (size == 2 && my_strcmp(args[1], "-h") == 0) {
+        help_message();
+        return 84;
     }
-    return 84;
+    navy = malloc(sizeof(navy_t));
+    navy->bool_game = 0;
+    navy->enemy_pid = 0;
+    navy->attack = malloc(sizeof(attack_t));
+    navy->attack->attack = malloc(sizeof(int) * 2);
+    handle_signal(SIGUSR1);
+    handle_signal(SIGUSR2);
+    fill_map(args[size - 1]);
+    if (size == 2) {
+        first_player();
+    } else {
+        navy->enemy_pid = my_atoi(args[1]);
+        second_player();
+    }
+    return 0;
 }
